8a9d80e8ad0b20f6154ecb5b6aea2de6ddb4a77f
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-02 20:13:10 +0700

    Task13

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+
+Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+int num = numberA/100;
+if( num == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (num > 0)
+{
+    Console.WriteLine(num%10);
+}
+else
+{
+    Console.WriteLine(num%10*-1);
+}
