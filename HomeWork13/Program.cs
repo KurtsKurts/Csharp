961990941f259455c5bc30605e8e8a688de11cdb
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-02 21:45:29 +0700

    Task15.1

--- HomeWork13/Program.cs
+++ HomeWork13/Program.cs
@@ -7,19 +7,47 @@
 
 32679 -> 6*/
 
-
 Console.WriteLine("Введите число: ");
 int numberA = int.Parse(Console.ReadLine());
-int num = numberA / 100;
-if (num == 0)
+
+if (numberA < 0)
+{
+    numberA = (numberA * (-1));
+}
+
+if (numberA < 100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
-else if (num > 0)
+else if (numberA < 1000)
 {
-    Console.WriteLine(num % 10);
+    Console.WriteLine(numberA % 10);
 }
-else
+else if (numberA < 10000)
 {
-    Console.WriteLine(num % 10 * -1);
+    Console.WriteLine(numberA % 100 / 10);
+}
+else if (numberA < 100000)
+{
+    Console.WriteLine(numberA % 1000 / 100);
+}
+else if (numberA < 1000000)
+{
+    Console.WriteLine(numberA % 10000 / 1000);
+}
+else if (numberA < 10000000)
+{
+    Console.WriteLine(numberA % 100000 / 10000);
+}
+else if (numberA < 100000000)
+{
+    Console.WriteLine(numberA % 1000000 / 100000);
+}
+else if (numberA < 1000000000)
+{
+    Console.WriteLine(numberA % 10000000 / 1000000);
+}
+else if (numberA < 10000000000)
+{
+    Console.WriteLine(numberA % 100000000 / 10000000);
 }
