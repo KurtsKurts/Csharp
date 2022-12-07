057269f9bf1b7244307dc9312c42a93a93f96c71
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-07 14:17:32 +0300

    Task19

---
+++ HomeWork19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19: Напишите программу, которая принимает
+// на вход пятизначное число и проверяет, является ли
+// оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int num1 = number / 10000;
+int num5 = number % 10;
+int num2 = number % 10000 / 1000;
+int num4 = number % 100 / 10;
+
+if (num1 == num5 & num2 == num4)
+{
+    Console.WriteLine("да");
+}
+else Console.WriteLine("нет");
