01dcb12b6bccd67292a8bec28152def9a5cd449c
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-13 22:56:41 +0700

    Сумма нечетных позиций

---
+++ HomeWork36/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0   */
+
+int[] RandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+int UnEvenSum(int[] arr)
+{
+    Console.WriteLine();
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        sum = sum + arr[i];
+    }
+    Console.WriteLine($"Сумма элементов на нечетных позициях = {sum}");
+    return sum;
+}
+
+UnEvenSum(RandomArray(8));
