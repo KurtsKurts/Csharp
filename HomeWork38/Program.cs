0c17f8c70dd7682c51f7b7e521c20e57f469be0f
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-14 00:30:34 +0700

    Разница мин макс обычных чисел

---
+++ HomeWork38/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+int[] RandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+// RandomArray(8);
+
+int MaxMinDiff(int[] arr)
+{
+    Console.WriteLine();
+    int min = arr[0];
+    int max = arr[0];
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+         max = arr[i];
+        if (arr[i] < min)
+         min = arr[i];
+    }
+    Console.WriteLine($"max = {max}");
+    Console.WriteLine($"min = {min}");
+    Console.WriteLine($"Разница между MAX и MIN элементов = {max-min}");
+    return max;
+    return min;
+    Console.WriteLine(min);
+}
+
+MaxMinDiff(RandomArray(8));
