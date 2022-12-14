4a62ef783a57a6707819f3e6c01f51d2b2560b5d
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-14 16:43:37 +0700

    Решение через While

--- HomeWork27/Program.cs
+++ HomeWork27/Program.cs
@@ -9,18 +9,27 @@
 
 int SumNums(int number)
 {
+
     int sum = 0;
-    int num1 = number / 10000000;
-    int num2 = number % 10000000 / 1000000;
-    int num3 = number % 1000000 / 100000;
-    int num4 = number % 100000 / 10000;
-    int num5 = number % 10000 / 1000;
-    int num6 = number % 1000 / 100;
-    int num7 = number % 100 / 10;
-    int num8 = number % 10;
-    sum = num1 + num2 + num3 + num4 + num5+num6+num7+num8;
-    //Console.WriteLine($"{num1}+{num2}+{num3}+{num4}+{num5}+{num6}+{num7}+{num8}");
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
     return sum;
+
+    // int sum = 0;
+    // int num1 = number / 10000000;
+    // int num2 = number % 10000000 / 1000000;
+    // int num3 = number % 1000000 / 100000;
+    // int num4 = number % 100000 / 10000;
+    // int num5 = number % 10000 / 1000;
+    // int num6 = number % 1000 / 100;
+    // int num7 = number % 100 / 10;
+    // int num8 = number % 10;
+    // sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8;
+    // //Console.WriteLine($"{num1}+{num2}+{num3}+{num4}+{num5}+{num6}+{num7}+{num8}");
+    // return sum;
 }
 
 
