e2a0474c0ad3b7be706b46f31f392964ffdd4683
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-17 23:23:21 +0700

    Кол-во положит чисел

---
+++ HomeWork41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write("Введите числа через 'Space'' : ");
+int[] Num = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+void PosNum(int[] num)
+{
+    int count = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] > 0)
+            count++;
+    }
+    Console.WriteLine(count);
+}
+Console.Write($"Количество положительных чисел -> ");
+PosNum(Num);
