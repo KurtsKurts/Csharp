6edcff8e817d9f5e3e24abbd8bb9b6bd26c5dfd7
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-22 15:56:31 +0700

    AverageComumn

---
+++ HomeWork52/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageColumn(int[,] arrayWork)
+{
+    Console.WriteLine();
+    for (int i = 0; i < arrayWork.GetLength(1); i++)
+    {
+        double sumRow = 0;
+        for (int j = 0; j < arrayWork.GetLength(0); j++)
+        {
+            sumRow += arrayWork[j, i];
+        }
+        double averageColumn = Math.Round(sumRow/arrayWork.GetLength(0),2);
+        Console.WriteLine($"{averageColumn} ");
+    }
+}
+
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns);
+
+PrintArray(array);
+AverageColumn(array);
