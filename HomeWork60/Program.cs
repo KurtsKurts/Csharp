3d5304b03d2b620205883d0bc528a5c7e053a5ad
“KurtsKurts” <Alex23.89@mail.ru>
2022-12-24 21:59:34 +0700

    3D_Array

---
+++ HomeWork60/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+ Напишите программу, которая будет построчно выводить массив, 
+ добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+int[,,] GetArray(int m, int n, int l)
+{
+    int[,,] result = new int[m, n, l];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < l; k++)
+            {
+                result[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.Write($"{inArray[i, j,k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите глубину массива: ");
+int deep = int.Parse(Console.ReadLine()!);
+
+int[,,] array = GetArray(rows, columns, deep);
+
+PrintArray(array);
