b84a0aaf5f2516961724862627ff36bb0c7898e8
“KurtsKurts” <Alex23.89@mail.ru>
2023-01-06 18:13:39 +0700

    SumNub

---
+++ HomeWork66/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов
+в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int SumNaturNumb(int m, int n, int sum = 0)
+{
+    if (m <= n) sum = m + SumNaturNumb(m + 1, n);
+    return sum;
+}
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.Write(SumNaturNumb(M, N));
